dbc100ab77338ffa003a4a9eed47dcab47e47ee6
coilwhine <hen.daniel47@gmail.com>
2024-02-04 13:23:26 +0200

    gogle aouth

--- src/Components/LoginPage/LoginPage.tsx
+++ src/Components/LoginPage/LoginPage.tsx
@@ -3,9 +3,21 @@ import "./LoginPage.scss";
 
 function LoginPage(): JSX.Element {
 
+    function googleNavigate(url: string) {
+        window.location.href = url;
+    }
+
+    async function testbtn() {
+        const response = await fetch("http://localhost:3001/oauth",
+            { method: "post" });
+        const data = await response.json();
+
+        googleNavigate(data.url);
+    }
+
     return (
         <div className="LoginPage">
-
+            <button onClick={() => testbtn()}>test btn</button>
         </div>
     );
 }
