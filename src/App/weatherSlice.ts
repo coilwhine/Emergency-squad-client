6b9d2da8babab72ff843b4812a6f522fee19dae8
coilwhine <hen.daniel47@gmail.com>
2024-02-05 19:34:42 +0200

    add redux

---
+++ src/App/weatherSlice.ts
@@ -0,0 +1,17 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+let initialState = null;
+
+export const weatherSlice = createSlice({
+    name: 'weatherData',
+    initialState,
+    reducers: {
+        getWeather: (state, action: PayloadAction<any>) => {
+            state = action.payload;
+            return state;
+        }
+    }
+})
+
+export const { getWeather } = weatherSlice.actions
+export default weatherSlice.reducer
